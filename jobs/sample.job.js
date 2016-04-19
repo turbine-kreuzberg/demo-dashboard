d6d4587cc7a3c92c2175b6b0ebd944a48cd92daa
Matthias Gronwald <matthias.gronwald@votum.de>
2016-04-20 01:24:08 +0200

    Add health widgets with fake data

--- jobs/sample.job.js
+++ jobs/sample.job.js
@@ -1,6 +1,8 @@
 var current_valuation = 0;
 var current_karma = 0;
 send_event('coverage', { value: 63, background: 'green' });
+send_event('health-stage', { criticals: 0, warnings: 3 });
+send_event('health-prod', { criticals: 0, warnings: 0 });
 
 setInterval(function() {
   var last_valuation = current_valuation;
@@ -11,4 +13,6 @@ setInterval(function() {
   send_event('valuation', {current: current_valuation, last: last_valuation});
   send_event('karma', {current: current_karma, last: last_karma});
   // send_event('synergy', {value: Math.floor(Math.random() * 100)});
+  send_event('health-stage', { criticals: 0, warnings: 3 });
+  send_event('health-prod', { criticals:0, warnings: 0 });
 }, 2 * 1000);
